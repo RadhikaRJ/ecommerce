7d52e6b8659fbe963dafa3fe6b29341b3707ef97
RadhikaRJ <radhika7002@gmail.com>
2021-09-23 13:53:28 +0530

    feat: redirect user to login if not logged in

--- src/pages/wishlistedItemsPage.js
+++ src/pages/wishlistedItemsPage.js
@@ -4,7 +4,7 @@ import "../styles/productCard.css";
 import "../styles/button.css";
 import { REMOVE_FROM_WISHLIST } from "../constants/constants";
 import ButtonAddToCart from "../displayComponents/addToCartButton/addToCart";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, Navigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 function WishListdisplay() {
@@ -12,71 +12,79 @@ function WishListdisplay() {
   const navigate = useNavigate();
   return (
     <div>
-      {state.wishlist.length === 0 ? (
+      {localStorage.getItem("TOKEN") ? (
         <div>
-          <h3>You don't have any items in your wish list</h3>
-          <button
-            class="btn-text btn-style-color"
-            onClick={() => navigate("/products")}
-          >
-            Go back to view products
-          </button>
-        </div>
-      ) : (
-        <h3>Your wishlisted items:</h3>
-      )}
+          {state.wishlist.length === 0 ? (
+            <div>
+              <h3>You don't have any items in your wish list</h3>
+              <button
+                class="btn-text btn-style-color"
+                onClick={() => navigate("/products")}
+              >
+                Go back to view products
+              </button>
+            </div>
+          ) : (
+            <h3>Your wishlisted items:</h3>
+          )}
 
-      <div className="product-display-list-container">
-        {state.wishlist.map((item) => {
-          return (
-            <div className="product-item-display " key={item._id}>
-              <div class="card-container">
-                <div class="card-large">
-                  <button
-                    class="card-remove-btn"
-                    onClick={() =>
-                      dispatch({ type: REMOVE_FROM_WISHLIST, item })
-                    }
-                  >
-                    &times;
-                  </button>
-                  <div class="card-content">
-                    <img
-                      class="image-card-size-large image-size-setter"
-                      src={item.product_id.url}
-                      width="100%"
-                      height="auto"
-                      alt={item.product_id.name}
-                    />
-                    <h3> {item.product_id.name} </h3>
-                    <div>Rs. {item.product_id.price}</div>
-                    {item.product_id.availability && <div> In Stock </div>}
-                    {!item.product_id.availability && <div> Out of Stock </div>}
+          <div className="product-display-list-container">
+            {state.wishlist.map((item) => {
+              return (
+                <div className="product-item-display " key={item._id}>
+                  <div class="card-container">
+                    <div class="card-large">
+                      <button
+                        class="card-remove-btn"
+                        onClick={() =>
+                          dispatch({ type: REMOVE_FROM_WISHLIST, item })
+                        }
+                      >
+                        &times;
+                      </button>
+                      <div class="card-content">
+                        <img
+                          class="image-card-size-large image-size-setter"
+                          src={item.product_id.url}
+                          width="100%"
+                          height="auto"
+                          alt={item.product_id.name}
+                        />
+                        <h3> {item.product_id.name} </h3>
+                        <div>Rs. {item.product_id.price}</div>
+                        {item.product_id.availability && <div> In Stock </div>}
+                        {!item.product_id.availability && (
+                          <div> Out of Stock </div>
+                        )}
 
-                    {item.product_id.fast_delivery ? (
-                      <div> Fast Delivery </div>
-                    ) : (
-                      <div> 3 days minimum </div>
-                    )}
+                        {item.product_id.fast_delivery ? (
+                          <div> Fast Delivery </div>
+                        ) : (
+                          <div> 3 days minimum </div>
+                        )}
+                      </div>
+                    </div>
+
+                    <div class="card-large-showSimilar-section">
+                      <ButtonAddToCart item={item} />{" "}
+                      <button class="btn-text btn-style-color ">
+                        <Link
+                          to={`/products/${item.product_id._id}`}
+                          class="btn-style-color link-btn"
+                        >
+                          View Details
+                        </Link>
+                      </button>
+                    </div>
                   </div>
                 </div>
-
-                <div class="card-large-showSimilar-section">
-                  <ButtonAddToCart item={item} />{" "}
-                  <button class="btn-text btn-style-color ">
-                    <Link
-                      to={`/products/${item.product_id._id}`}
-                      class="btn-style-color link-btn"
-                    >
-                      View Details
-                    </Link>
-                  </button>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+              );
+            })}
+          </div>
+        </div>
+      ) : (
+        <Navigate to="/login" />
+      )}
     </div>
   );
 }
