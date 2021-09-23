c64307bc4d75f9ec7b203d3ad63b46a0ca2d5df7
RadhikaRJ <radhika7002@gmail.com>
2021-09-23 13:57:48 +0530

    feat: redirect to login page if user is not logged in

--- src/pages/cartItemsPage.js
+++ src/pages/cartItemsPage.js
@@ -4,7 +4,7 @@ import "../styles/productCard.css";
 import "../styles/button.css";
 import { REMOVE_FROM_CART } from "../constants/constants";
 import CartSummary from "../displayComponents/cartSummary/cartSummary";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Navigate } from "react-router-dom";
 import ButtonQuantityUpdate from "../displayComponents/buttonQuantityUpdate/buttonQuantityUpdate";
 
 function Cartdisplay() {
@@ -14,77 +14,83 @@ function Cartdisplay() {
 
   return (
     <div>
-      {(state.cart.length !== 0 && <h2>Your Cart</h2>) || (
-        <h2>Your cart is empty</h2>
-      )}
-      {(state.cart.length !== 0 && (
-        <button
-          class="btn-text btn-style-color"
-          onClick={() => navigate("/checkout")}
-        >
-          Checkout
-        </button>
-      )) || (
-        <button
-          class="btn-text btn-style-color"
-          onClick={() => navigate("/products")}
-        >
-          Go back to view products
-        </button>
-      )}
-      {state.cart.length !== 0 && (
-        <div className="cart-container">
-          <div></div>
-          <div className="product-display-list-container cart-items-section">
-            {state.cart.map((item) => {
-              return (
-                <div className="product-item-display" key={item._id}>
-                  <div class="card-container">
-                    <div class="card-medium">
-                      <button
-                        class="card-remove-btn"
-                        onClick={() =>
-                          dispatch({ type: REMOVE_FROM_CART, item })
-                        }
-                      >
-                        &times;
-                      </button>
-                      <div class="card-content">
-                        <img
-                          class="image-card-size-medium image-size-setter"
-                          src={item.itemInCart_id.url}
-                          width="100%"
-                          height="auto"
-                          alt={item.itemInCart_id.name}
-                        />
-                        <h3> {item.itemInCart_id.name} </h3>
-                        <div>Rs. {item.itemInCart_id.price}</div>
-                        {item.itemInCart_id.availability && (
-                          <div> In Stock </div>
-                        )}
-                        {!item.itemInCart_id.availability && (
-                          <div> Out of Stock </div>
-                        )}
-                        <div>{item.itemInCart_id.level}</div>
-                        {item.itemInCart_id.fast_delivery ? (
-                          <div> Fast Delivery </div>
-                        ) : (
-                          <div> 3 days minimum </div>
-                        )}
+      {localStorage.getItem("TOKEN") ? (
+        <div>
+          {(state.cart.length !== 0 && <h2>Your Cart</h2>) || (
+            <h2>Your cart is empty</h2>
+          )}
+          {(state.cart.length !== 0 && (
+            <button
+              class="btn-text btn-style-color"
+              onClick={() => navigate("/checkout")}
+            >
+              Checkout
+            </button>
+          )) || (
+            <button
+              class="btn-text btn-style-color"
+              onClick={() => navigate("/products")}
+            >
+              Go back to view products
+            </button>
+          )}
+          {state.cart.length !== 0 && (
+            <div className="cart-container">
+              <div></div>
+              <div className="product-display-list-container cart-items-section">
+                {state.cart.map((item) => {
+                  return (
+                    <div className="product-item-display" key={item._id}>
+                      <div class="card-container">
+                        <div class="card-medium">
+                          <button
+                            class="card-remove-btn"
+                            onClick={() =>
+                              dispatch({ type: REMOVE_FROM_CART, item })
+                            }
+                          >
+                            &times;
+                          </button>
+                          <div class="card-content">
+                            <img
+                              class="image-card-size-medium image-size-setter"
+                              src={item.itemInCart_id.url}
+                              width="100%"
+                              height="auto"
+                              alt={item.itemInCart_id.name}
+                            />
+                            <h3> {item.itemInCart_id.name} </h3>
+                            <div>Rs. {item.itemInCart_id.price}</div>
+                            {item.itemInCart_id.availability && (
+                              <div> In Stock </div>
+                            )}
+                            {!item.itemInCart_id.availability && (
+                              <div> Out of Stock </div>
+                            )}
+                            <div>{item.itemInCart_id.level}</div>
+                            {item.itemInCart_id.fast_delivery ? (
+                              <div> Fast Delivery </div>
+                            ) : (
+                              <div> 3 days minimum </div>
+                            )}
 
-                        <ButtonQuantityUpdate item={item} />
+                            <ButtonQuantityUpdate item={item} />
+                          </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+                  );
+                })}
+              </div>
 
-          <div className="cart-summary-section">
-            <CartSummary />
-          </div>
+              <div className="cart-summary-section">
+                <CartSummary />
+              </div>
+            </div>
+          )}
         </div>
+      ) : (
+        <Navigate to="/login" />
       )}
     </div>
   );
